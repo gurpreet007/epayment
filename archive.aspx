96843500c29166b35fb2e443621b07293d7f4d8b
Gurpreet <gurpreet007@gmail.com>
2016-04-21 20:07:18 +0530

    Implemented Archive Bill Functionality

---
+++ archive.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="archive.aspx.cs" Inherits="archive" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <meta charset="utf-8">
+    <title>e-Payment</title>
+    <!--[if lt IE 9]>
+        <script src="scripts/html5shiv.min.js"></script>        
+   <![endif]-->
+    <link rel="stylesheet/less" href="styles/epayment.less" type="text/css" />
+    <script src="scripts/less.min.js"></script>
+</head>
+<body> 
+    <header class="page" id="pageHeader">	</header>
+    <div id="loginInfo">
+        <asp:Label ID="lblLoggedInAs" runat="server"></asp:Label> 
+    </div>
+    <nav id="pageNav"></nav>
+    <header class="sectionHeader">Archive Bill</header>
+    <form id="Form1" runat="server" class="tableWrapper">
+        <div class="tableRow">
+            <p></p>
+            <p style="margin-left:0px; padding-left:0px;"><input type="text" runat="server" id="txtAcno" maxlength="12" placeholder="Non-SAP Ac. No."/></p>
+        </div>
+        <div class="tableRow">
+            <p></p>
+            <asp:Button ID="btnShow" Text="Show Info" runat="server" onclientclick="$('#lblMsg').text('')" onclick="btnShow_Click" />
+        </div>
+        <div class="tableRow">
+            <p>Ac. No.:</p>
+            <p><asp:Label ID="lblAcNo" runat="server"></asp:Label></p>
+        </div>
+        <div class="tableRow">
+            <p>Name:</p>
+            <p><asp:Label ID="lblName" runat="server"></asp:Label></p>
+        </div>
+        <div class="tableRow">
+            <p>Category:</p>
+            <p><asp:Label ID="lblCat" runat="server"></asp:Label></p>
+        </div>
+        <div class="tableRow">
+            <p>Date Upload:</p>
+            <p><asp:Label ID="lblDtUpload" runat="server"></asp:Label></p>
+        </div>
+        <div class="tableRow">
+            <p></p>
+            <p><asp:Label ID="lblMsg" class="msg" runat="server"></asp:Label></p>
+        </div>
+        <div class="tableRow">
+            <p></p>
+            <p><asp:Button ID="btnArchive" Text="Archive Bill" runat="server" onclientclick="$('#lblMsg').text('')" onclick="btnArchive_Click" Visible="False" /></p>
+        </div>
+    </form>
+    <footer id="pageFooter" class="pageFooter">	</footer>
+    <script src="scripts/jquery-2.2.0.min.js"></script>
+    <script src="scripts/common.js"></script>
+    <script>
+        $(function () {
+            $("#pageHeader").load("resources/snippets.html #snipPageHeader")
+            $("#pageNav").load("resources/snippets.html #snipPageNav", function () {
+                $("#pageNav li").removeClass("selected");
+                $("#pageNav #nvDownload").addClass("selected");
+                $("#pageNav").hover(
+                    function () { $("#pageNav li").removeClass("selected"); },
+                    function () { $("#pageNav #nvArchive").addClass("selected"); }
+                );
+            });
+            $("#pageFooter").load("resources/snippets.html #snipPageFooter");
+        });
+    </script>
+</body>
+</html>
---
+++ archive.aspx.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class archive : System.Web.UI.Page
+{
+
+    private void Clear()
+    {
+        lblAcNo.Text = string.Empty;
+        lblName.Text = string.Empty;
+        lblCat.Text = string.Empty;
+        lblDtUpload.Text = string.Empty;
+    }
+    protected void btnShow_Click(object sender, EventArgs e)
+    {
+        string acno = txtAcno.Value;
+        string sql = string.Empty;
+        DataSet ds;
+
+        lblMsg.Text = string.Empty;
+
+        Clear();        
+        if (String.IsNullOrEmpty(acno) || acno.Length != 12)
+        {
+            lblMsg.Text = "Invalid Account Number";
+            return;
+        }
+        sql = String.Format("select cname, table_name, to_char(updatedt, 'dd-Mon-yyyy hh24:mi:ss') as dtup, if_sap from mast_account where account_no = '{0}'", acno);
+        ds = OraDBConnection.GetData(sql);
+
+        if (ds.Tables[0].Rows.Count != 1)
+        {
+            lblMsg.Text = "No such account number";
+            return;
+        }
+        else if (ds.Tables[0].Rows[0]["if_sap"].ToString() != "N")
+        {
+            lblMsg.Text = "Only enter Non-SAP Account Numbers";
+            return;
+        }
+
+        lblAcNo.Text = acno;
+        lblName.Text = ds.Tables[0].Rows[0]["cname"].ToString();
+        lblCat.Text = ds.Tables[0].Rows[0]["table_name"].ToString().Split('.')[1].ToUpper();
+        lblDtUpload.Text = ds.Tables[0].Rows[0]["dtup"].ToString();
+        btnArchive.Visible = true;
+    }
+    protected void btnArchive_Click(object sender, EventArgs e)
+    {
+        string acno = lblAcNo.Text;
+        string tbl = lblCat.Text;
+        string userid = Session[common.strUserID].ToString();
+        string sql = string.Empty;
+        string sqlCond = string.Empty;
+        string sqlDel = string.Empty;
+        string sqlIns = string.Empty;
+        string sqlLog = string.Empty;
+        string year = DateTime.Now.Year.ToString();
+        DataSet ds;
+        Dictionary<string, string> dict_acno = new Dictionary<string, string>(){
+                                                   {"DSBELOW10KW","ACCOUNTNO"},
+                                                   {"DSABOVE10KW","ACCOUNTNO"},
+                                                   {"LS","ACCOUNTNO"},
+                                                   {"MS","PRT_AC_NO"},
+                                                   {"SP","ACNO"}
+                                               };
+
+        if (!dict_acno.ContainsKey(tbl))
+        {
+            lblMsg.Text = "Invalid Category";
+            return;
+        }
+
+        sqlCond = string.Format(" {0} = '{1}' and userid = '{2}' ", dict_acno[tbl], acno, userid);
+        sql = string.Format("select count(*) from {0} where {1}", tbl, sqlCond);
+        ds = OraDBConnection.GetData(sql);
+
+        if (ds.Tables[0].Rows[0][0].ToString() != "1")
+        {
+            lblMsg.Text = "No current bill or not uploaded by this user";
+            return;
+        }
+
+        sqlIns = string.Format("insert into ARCdBILL.ARC_{0}_{1} select * from {1} where {2}", year, tbl, sqlCond);
+        sqlDel = string.Format("delete from {0} where {1}", tbl, sqlCond);
+        sqlLog = string.Format("insert into ARCHIVEBILL_LOG values ('{0}','{1}', sysdate)", acno, userid);
+        sql = string.Format("BEGIN {0}; {1}; {2}; END;", sqlIns, sqlDel, sqlLog);
+        try
+        {
+            OraDBConnection.ExecQry(sql);
+            lblMsg.Text = "Record Archived";
+        }
+        catch (Exception ex)
+        {
+            lblMsg.Text = ex.Message;
+        }
+    }
+}
